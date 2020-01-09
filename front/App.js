fbafd7fadc469c71b0ed14bf5d209e9faa88f1ef
= <jacex17@gmail.com>
2020-01-09 00:24:33 -0500

    add signup and login screens

--- front/App.js
+++ front/App.js
@@ -1,65 +1,106 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {Provider, Consumer } from './hookAndContext/context';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer'
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import HomeScreen from './components/HomeScreen';
-import { createAppContainer } from 'react-navigation';
-import SearchAuct from './components/SearchAuct';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import HomeScreen from './screens/HomeScreen';
+import MainScreen from './screens/MainScreen';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import SearchAuct from './screens/SearchAuct';
+
+
+class App extends Component {
+
+  render(){
+    return <AppContainer/>
+  }
+}
 
 const authStack = createStackNavigator({
-  Login: { 
-    screen: Login 
-  },
-  Register: { 
-    screen: Signup 
-  }
-});
-
-const SearchStack = createStackNavigator({
-  Search: {
-    screen: ({screenProps, navigation}) => <Consumer>{context => <SearchAuct data={context} navigation={navigation}/>}</Consumer>
+  Welcome: {screen: MainScreen},
+  Login: {screen: Login},
+  Signup: {screen: Signup},
+}, {
+  defaultNavigationOptions:{
+    headerTitle: ""
   }
 })
 
-const HomeStack = createStackNavigator({
-  Home: {
-    screen: ({screenProps, navigation}) => <Consumer>{context => <HomeScreen data={context} navigation={navigation}/>}</Consumer>
-  },
-  Hello: {
-    screen: ({screenProps, navigation}) => <Consumer>{context => <HomeScreen data={context} navigation={navigation}/>}</Consumer>
-  }
+const AppSwitchNavigator = createSwitchNavigator({
+  Welcome: {screen: authStack},
 })
 
-const BottomStack = createBottomTabNavigator({
-  Home: {
-    screen: HomeStack
-  },
-  Search: {
-    screen: SearchStack
-  },
-});
+const AppContainer = createAppContainer(AppSwitchNavigator);
 
-const AppNavigator = createDrawerNavigator({
-  Auth: {
-    screen: authStack
-  },
-  Footer: {
-    screen: BottomStack
-  }
-});
+export default App;
 
-const Nav = createAppContainer(AppNavigator)
 
-const App = createBottomTabNavigator({
-  Home: {screen: HomeStack},
-  Search: { screen: SearchStack},
-  Settings: { screen: HomeStack},
-})
+
+
+
+
+
+
+
+
+
+
+
+//OLD VERSION
+
+// const authStack = createStackNavigator({
+//   Login: { 
+//     screen: Login 
+//   },
+//   Register: { 
+//     screen: Signup 
+//   }
+// });
+
+// const SearchStack = createStackNavigator({
+//   Search: {
+//     screen: ({screenProps, navigation}) => <Consumer>{context => <SearchAuct data={context} navigation={navigation}/>}</Consumer>
+//   }
+// })
+
+// const HomeStack = createStackNavigator({
+//   Home: {
+//     screen: ({screenProps, navigation}) => <Consumer>{context => <HomeScreen data={context} navigation={navigation}/>}</Consumer>
+//   },
+//   Hello: {
+//     screen: ({screenProps, navigation}) => <Consumer>{context => <HomeScreen data={context} navigation={navigation}/>}</Consumer>
+//   }
+// })
+
+// const BottomStack = createBottomTabNavigator({
+//   Home: {
+//     screen: HomeStack
+//   },
+//   Search: {
+//     screen: SearchStack
+//   },
+// });
+
+// const AppNavigator = createDrawerNavigator({
+//   Auth: {
+//     screen: authStack
+//   },
+//   Footer: {
+//     screen: BottomStack
+//   }
+// });
+
+// const Nav = createAppContainer(AppNavigator)
+
+// const App = createBottomTabNavigator({
+//   Home: {screen: HomeStack},
+//   Search: { screen: SearchStack},
+//   Settings: { screen: HomeStack},
+// })
 
 //  const App = (props) => {
 //   return (
@@ -75,4 +116,4 @@ const App = createBottomTabNavigator({
 // }
 
 
-export default createAppContainer(App);
+// export default createAppContainer(App);
---
+++ front/screens/MainScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const MainScreen = (props) => {
+    return(
+        <View style={styles.container}>
+        <Button title='Log In' onPress={() => props.navigation.navigate('Login')}/>
+        <Button title='Sign Up' onPress={() => props.navigation.navigate('Signup')}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default MainScreen;
