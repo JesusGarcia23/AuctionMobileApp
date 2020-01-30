b12f9b68435e1293a6fe9b4982f360230ecad611
= <jacex17@gmail.com>
2020-01-30 00:07:12 -0500

    Remove old commented code

--- front/hookAndContext/context.js
+++ front/hookAndContext/context.js
@@ -200,19 +200,3 @@ const data = {
 export const ContextConsumer = Context.Consumer
 
 export { Context , Provider }
-
-// const uploadNewImage = async (e) => {
-//     e.preventDefault();
-//     console.log(imageUpload)
-//     const time = new Date();
-//     console.log(time)
-//     const imageName = `${currentUser.firstName}${imageUpload.name}`
-//     imageUpload.owner = imageName;
-//     const uploadData = new FormData();
-//     await uploadData.append("imageUrl", imageUpload)
-//     console.log(uploadData)
-    // api.post('/uploadNewImg', uploadData, {withCredentials: true})
-    // .then(response => {
-    //     console.log(response)
-    // }).catch(err => console.error(err))
-//}
